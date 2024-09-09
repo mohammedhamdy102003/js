1817d19e5b50ab0c9a7676586715ec6f02ec91e7
mohammed hamdy <mohammedhamdy102003@gmail.com>
2024-09-09 23:58:21 +0300

    message

---
+++ swiper.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullests: true,
+      clickable:true
+    },
+    autoplay:{
+        delay:2000,
+    },
+    loop:true,
+  });
+  var swiper = new Swiper(".sale-sac", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullests: true,
+      clickable:true
+    },
+    slidesPerView:5,
+    spaceBetween:30,
+    autoplay:{
+        delay:2000,
+    },
+    loop:true,
+  });
+
+  var swiper = new Swiper(".product_swip", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullests: true,
+      clickable:true
+    },
+    slidesPerView:4,
+    spaceBetween:30,
+    autoplay:{
+        delay:2000,
+    },
+    loop:true,
+  });
